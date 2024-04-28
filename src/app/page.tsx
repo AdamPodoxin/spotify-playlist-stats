d0ee320e8390bd3925ac29d87e80092fe944f68d
Adam Podoxin <adam.podoxin@gmail.com>
2024-04-28 10:25:57 -0700

    Wrap home page in suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import HomePage from "./components/HomePage";
 
 const Home = () => {
 	return (
 		<>
 			<main className="flex min-h-screen flex-col items-center  p-24 gap-4">
-				<HomePage />
+				<Suspense>
+					<HomePage />
+				</Suspense>
 			</main>
 		</>
 	);
