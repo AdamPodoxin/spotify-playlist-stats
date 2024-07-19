ddb7a11cc1cc6cc005172209091bb1666dc2de4d
Adam Podoxin <adam.podoxin@gmail.com>
2024-07-19 00:21:50 -0700

    Styling changes

--- src/components/TracksByArtistList/index.tsx
+++ src/components/TracksByArtistList/index.tsx
@@ -12,7 +12,7 @@ const TracksByArtistList = ({
 
   return (
     <div>
-      <p className="my-4 text-2xl">Artists:</p>
+      <span className="my-4 text-2xl">Artists:</span>
       <div className="flex flex-col gap-4">
         {tracksForArtists.map(([artist, tracks]) => {
           const artistName = !!artist.name ? artist.name : "(no artist)";
@@ -21,23 +21,27 @@ const TracksByArtistList = ({
           const tracksString = tracks.length > 1 ? "tracks" : "track";
 
           return (
-            <div key={artist.id ?? artist.name}>
+            <div key={artist.id ?? artist.name} className="flex flex-col gap-1">
               {artistUrl && (
                 <a
                   href={artistUrl}
                   target="_blank"
                   referrerPolicy="no-referrer"
-                  className="my-1 border-b border-solid border-slate-50/50 text-xl"
+                  className="my-1 w-min text-nowrap bg-slate-50/10 text-xl hover:bg-slate-50/20"
                 >
                   {artistName}
                 </a>
               )}
 
-              {!artistUrl && <p className="my-1 text-xl">{artistName}</p>}
+              {!artistUrl && (
+                <span className="my-1 w-min text-nowrap text-xl">
+                  {artistName}
+                </span>
+              )}
 
-              <p className="mb-2 text-lg">
+              <span className="mb-2 w-min text-nowrap text-lg">
                 {tracks.length} {tracksString}
-              </p>
+              </span>
 
               <div className="flex flex-col gap-1">
                 {tracks.map((track) => {
@@ -52,7 +56,7 @@ const TracksByArtistList = ({
                           href={trackUrl}
                           target="_blank"
                           referrerPolicy="no-referrer"
-                          className="text-md border-b border-dashed border-slate-50/20"
+                          className="text-md border-b border-dashed border-slate-50/20 hover:border-slate-50/50"
                         >
                           {track.name}
                         </a>
