7e8102c7f4b363c00aa4351c3a67e030c7f38431
Adam Podoxin <adam.podoxin@gmail.com>
2024-07-19 19:10:08 -0700

    Implement refresh token

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,13 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { saveCodeVerifier } from "~/utils/spotifyToken";
+import {
+  getCurrentToken,
+  saveCodeVerifier,
+  saveToken,
+} from "~/utils/spotifyToken";
 import { env } from "~/env";
+import type { SpotifyTokenResponse } from "~/types";
 
 const scope = "user-read-private user-read-email playlist-read-private";
 const baseUrl = env.NEXT_PUBLIC_BASE_URL;
@@ -21,23 +26,54 @@ const sha256 = async (plain: string) => {
   return window.crypto.subtle.digest("SHA-256", data);
 };
 
-const base64encode = (input: ArrayBuffer) => {
+const base64encodeBuffer = (input: ArrayBuffer) => {
   return btoa(String.fromCharCode(...new Uint8Array(input)))
     .replace(/=/g, "")
     .replace(/\+/g, "-")
     .replace(/\//g, "_");
 };
 
+const base64encodeString = (input: string) => {
+  return btoa(input).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+};
+
+const tokenEndpoint = "https://accounts.spotify.com/api/token";
+
+const fetchRefreshToken = async (refreshToken: string) => {
+  const encodedAuthorization = base64encodeString(
+    `${env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}:${env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`,
+  );
+
+  const response = await fetch(tokenEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${encodedAuthorization}`,
+    },
+    body: new URLSearchParams({
+      client_id: env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+      grant_type: "refresh_token",
+      refresh_token: refreshToken,
+    }),
+  });
+
+  if (!response.ok) {
+    return null;
+  }
+
+  return (await response.json()) as SpotifyTokenResponse;
+};
+
 const LoginPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const authenticateUser = async () => {
+    const getAccessToken = async () => {
       const codeVerifier = generateRandomString(64);
       saveCodeVerifier(codeVerifier);
 
       const hashed = await sha256(codeVerifier);
-      const codeChallenge = base64encode(hashed);
+      const codeChallenge = base64encodeBuffer(hashed);
 
       const authorizationEndpoint = new URL(
         "https://accounts.spotify.com/authorize",
@@ -53,6 +89,25 @@ const LoginPage = () => {
       }).toString();
 
       router.push(authorizationEndpoint.toString());
+    };
+
+    const refreshAccessToken = async (refreshToken: string) => {
+      const newRefreshToken = await fetchRefreshToken(refreshToken);
+
+      if (newRefreshToken) {
+        saveToken(newRefreshToken);
+        router.push(baseUrl);
+      }
+    };
+
+    const authenticateUser = async () => {
+      const currentToken = getCurrentToken();
+
+      if (currentToken.access_token && currentToken.refresh_token) {
+        await refreshAccessToken(currentToken.refresh_token);
+      } else {
+        await getAccessToken();
+      }
     };
 
     void authenticateUser();
